88a9321632576f703babed33f3d0801581c13580
heyteacher <pioero@gmail.com>
2020-04-20 16:23:55 +0200

    add environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+
+  shareUrl : "https://heyteacher.github.io/COVID-19",
+  baseHref: "/COVID-19",
+
+  shareTags : "#coronavirus #coronavirus",
+
+  gTagConfig: 'UA-160361420-1',
+
+  otherLang: {
+    url: "/COVID-19/it/",
+    label: "IT"
+  }
+};
